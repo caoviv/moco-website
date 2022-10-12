e9f15c27f586c6f968a4ec7f1aec6a32f5b800ec
VivianC <viviancao58@gmail.com>
2022-10-12 15:52:55 +1100

    add toggle function to Facilities & Services Sect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Appears/Disappears the mobile menu
+// Toggles the mobile menu
 function toggleMenu() {
     let x = document.querySelector(".mobile-nav");
     if (x.style.display === "flex") {
@@ -8,3 +8,15 @@ function toggleMenu() {
     }
     console.log("Toggle Mobile Menu")
   }
+
+// Toggle Moco Facilities & Services Details
+
+function toggleDetail(service) {
+  const serviceText = document.querySelector(service);
+  if (serviceText.style.display === "block") {
+    serviceText.style.display = "none";
+  } else {
+    serviceText.style.display = "block";
+  }
+  console.log("Toggle Service Details")
+}
