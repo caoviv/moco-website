3c55dfa11c70549b5f4d57cb2f726339d35132b2
VivianC <viviancao58@gmail.com>
2023-02-13 15:22:30 +1100

    add toggle service detail function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 // Toggles the mobile menu
 function toggleMenu() {
-    let x = document.querySelector(".mobile-nav");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-    console.log("Toggle Mobile Menu")
+  let x = document.querySelector(".mobile-nav");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
   }
+  console.log("Toggle Mobile Menu");
+}
 
 // Toggle Moco Facilities & Services Details
 
 function toggleDetail(service) {
-  const serviceText = document.querySelector(service);
-  if (serviceText.style.display === "block") {
-    serviceText.style.display = "none";
+  const serviceText = document.querySelector(service).style.display;
+  if (serviceText === "block") {
+    serviceText = "none";
   } else {
-    serviceText.style.display = "block";
+    serviceText = "block";
   }
-  console.log("Toggle Service Details")
+  console.log("Toggle Service Details");
 }
